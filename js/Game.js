4d541e2e1fa3422f941fc1845991bafc88971d64
Yonatan Melka <ybmelka@gmail.com>
2020-02-24 18:21:02 -0600

    Add constructor to each class

---
+++ js/Game.js
@@ -0,0 +1,17 @@
+class Game {
+  constructor() {
+    this.board = new Board();
+    this.players = this.createPlayers();
+    this.ready = false;
+  }
+
+  /**
+   * Creates two player objects
+   * @return  {Array}    An array of two Player objects.
+   */
+  createPlayers() {
+    const player1 = new Player('Player 1', 1, '#e15258', true);
+    const player2 = new Player('Player 2', 2, '#e59a13', true);
+    return [player1, player2];
+  }
+}
---
+++ js/Token.js
@@ -0,0 +1,9 @@
+class Token {
+  constructor(owner) {
+    this.owner = owner;
+    this.id = `token-${index}-${owner.id}`;
+    this.dropped = false;
+  }
+
+  
+}
