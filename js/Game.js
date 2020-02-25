6ace178e7e30d1c05683d105220b9cbaf059a3de
Yonatan Melka <ybmelka@gmail.com>
2020-02-25 00:53:48 -0600

    Add check for winner

--- js/Game.js
+++ js/Game.js
@@ -68,4 +68,92 @@ class Game {
       activeToken.drop(targetSpace);
     }
   }
+
+  /**
+   * Checks if there a winner on the board after each token drop.
+   * @param   {Object}    Targeted space for dropped token.
+   * @return  {boolean}   Boolean value indicating whether the game has been won (true) or not (false)
+   */
+  checkForWin(target) {
+    const owner = target.token.owner;
+    let win = false;
+
+    // vertical
+    for (let x = 0; x < this.board.columns; x++) {
+      for (let y = 0; y < this.board.rows - 3; y++) {
+        if (
+          this.board.spaces[x][y].owner === owner &&
+          this.board.spaces[x][y + 1].owner === owner &&
+          this.board.spaces[x][y + 2].owner === owner &&
+          this.board.spaces[x][y + 3].owner === owner
+        ) {
+          win = true;
+        }
+      }
+    }
+
+    // horizontal
+    for (let x = 0; x < this.board.columns - 3; x++) {
+      for (let y = 0; y < this.board.rows; y++) {
+        if (
+          this.board.spaces[x][y].owner === owner &&
+          this.board.spaces[x + 1][y].owner === owner &&
+          this.board.spaces[x + 2][y].owner === owner &&
+          this.board.spaces[x + 3][y].owner === owner
+        ) {
+          win = true;
+        }
+      }
+    }
+
+    // diagonal
+    for (let x = 3; x < this.board.columns; x++) {
+      for (let y = 0; y < this.board.rows - 3; y++) {
+        if (
+          this.board.spaces[x][y].owner === owner &&
+          this.board.spaces[x - 1][y + 1].owner === owner &&
+          this.board.spaces[x - 2][y + 2].owner === owner &&
+          this.board.spaces[x - 3][y + 3].owner === owner
+        ) {
+          win = true;
+        }
+      }
+    }
+
+    // diagonal
+    for (let x = 3; x < this.board.columns; x++) {
+      for (let y = 3; y < this.board.rows; y++) {
+        if (
+          this.board.spaces[x][y].owner === owner &&
+          this.board.spaces[x - 1][y - 1].owner === owner &&
+          this.board.spaces[x - 2][y - 2].owner === owner &&
+          this.board.spaces[x - 3][y - 3].owner === owner
+        ) {
+          win = true;
+        }
+      }
+    }
+
+    return win;
+  }
+
+  /**
+   * Switches active player. If a player is active (true) switch it to false and vice versa
+   */
+  switchPlayers() {
+    this.players.forEach(player => (player.active = player.active ? false : true));
+  }
+
+  /**
+   * Displays game over message.
+   * @param {string} message - Game over message.
+   */
+  gameOver(message) {
+    document.getElementById('game-over').style.display = 'block';
+    document.getElementById('game-over').textContent = message;
+  }
+
+  updateGameState() {
+    
+  }
 }
--- js/Space.js
+++ js/Space.js
@@ -8,6 +8,18 @@ class Space {
     this.radius = this.diameter / 2;
   }
 
+  /**
+   * Checks if space has an associated token to find its owner
+   * @return  {(null|Object)} Returns null or the owner object of the space's associated token.
+   */
+  get owner() {
+    if (this.token === null) {
+      return null;
+    } else {
+      return this.token.owner;
+    }
+  }
+
   drawSVGSpace() {
     const svgSpace = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
     svgSpace.setAttributeNS(null, 'id', this.id);
@@ -16,7 +28,15 @@ class Space {
     svgSpace.setAttributeNS(null, 'r', this.radius - 8);
     svgSpace.setAttributeNS(null, 'fill', 'black');
     svgSpace.setAttributeNS(null, 'stroke', 'none');
-    
-    document.getElementById("mask").appendChild(svgSpace);  
+
+    document.getElementById('mask').appendChild(svgSpace);
+  }
+
+  /**
+   * Updates space to reflect a token has been dropped into it.
+   * @param {Object} token - The dropped token
+   */
+  mark(token) {
+    this.token = token;
   }
 }
